e1e09303a3ead1fd8600595883ae05d21f2dec44
LeonardoScalabrini <leonardo_scalabrini@hotmail.com>
2021-04-29 22:17:09 -0300

    Criado estrutura inicial projeto

---
+++ src/test/java/com/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ChatApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
