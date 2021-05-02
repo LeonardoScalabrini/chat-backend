c4dcdb00302d72fa8d91605c6ff7097041c80757
leonardoscalabrini <leonardo_scalabrini@hotmail.com>
2021-05-02 17:01:43 -0300

    Implementado namespace para salas

--- src/main/java/com/chat/ChatApplication.java
+++ src/main/java/com/chat/ChatApplication.java
@@ -17,12 +17,21 @@ public class ChatApplication {
         config.setPort(9092);
 
         final SocketIOServer server = new SocketIOServer(config);
-        server.addEventListener("chatevent", ChatObject.class, new DataListener<ChatObject>() {
+        final SocketIONamespace chat1namespace = server.addNamespace("/chat1");
+        chat1namespace.addEventListener("message", ChatObject.class, new DataListener<ChatObject>() {
             @Override
             public void onData(SocketIOClient client, ChatObject data, AckRequest ackRequest) {
-                // broadcast messages to all clients
                 System.out.println(data.getMessage());
-                server.getBroadcastOperations().sendEvent("chatevent", data);
+                chat1namespace.getBroadcastOperations().sendEvent("message", data);
+            }
+        });
+
+        final SocketIONamespace chat2namespace = server.addNamespace("/chat2");
+        chat2namespace.addEventListener("message", ChatObject.class, new DataListener<ChatObject>() {
+            @Override
+            public void onData(SocketIOClient client, ChatObject data, AckRequest ackRequest) {
+                System.out.println(data.getMessage());
+                chat2namespace.getBroadcastOperations().sendEvent("message", data);
             }
         });
 
